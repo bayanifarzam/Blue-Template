27deeb1d3c462e9e5d8490ea0c15dd433dc8f7cf
Raza Farzam <bayanifarzam@gmail.com>
2021-03-29 11:56:33 +0500

    jQuery added

--- assets/JS/custom.js
+++ assets/JS/custom.js
@@ -1,1 +1,26 @@
-alert('Hello from custom.js')
+$(function(){
+
+
+
+
+// $(".more").click(function(){
+//     $(".moreNav").addClass('d-md-block');
+// });
+
+
+$('.more').on('click',function(){
+
+    if($('.moreNav').hasClass("d-none")){
+        $(".moreNav").removeClass('d-none');
+ 
+
+    }else{
+        $('.moreNav').addClass('d-none');
+    }
+    
+});
+
+
+
+
+});
