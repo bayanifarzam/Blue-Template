14f31afe9c305cb7ee8c39bce4b6d565217e76d0
Raza Farzam <bayanifarzam@gmail.com>
2021-03-30 12:21:27 +0500

    jquery

--- assets/JS/custom.js
+++ assets/JS/custom.js
@@ -1,6 +1,6 @@
 $(function(){
 
-
+// $('.moreNav').hide();
 
 
 // $(".more").click(function(){
@@ -10,8 +10,13 @@ $(function(){
 
 $('.more').on('click',function(){
 
+
+
+
+
     if($('.moreNav').hasClass("d-none")){
         $(".moreNav").removeClass('d-none');
+        $(".moreNav").fadeIn('slow');
  
 
     }else{
@@ -19,6 +24,48 @@ $('.more').on('click',function(){
     }
     
 });
+
+
+
+
+
+// $('.s1').hover(function(){
+//    $(this).removeClass('p1');
+//    $(this).addClass('bg-primary')
+// })
+
+$('.t').hide()
+
+$('.s1').on({
+            mouseenter: function(){
+                $(this).removeClass('p1');
+                $(this).addClass('bg-primary')
+                $('.t').fadeIn('slow');
+              
+                // $(this).text('This is some text.')
+            },
+            mouseleave: function(){
+                $(this).addClass('p1');
+                $(this).removeClass('bg-primary')
+                $('.t').fadeOut('fast');
+
+                // $(this).text('')
+            }
+
+})
+
+$('.s2').on({
+    mouseenter: function(){
+        $('.he').removeClass('d-none');
+
+        // $(this).text('This is some text.')
+    },
+    mouseleave: function(){
+        $('.he').addClass('d-none');
+        // $(this).text('')
+    }
+
+})
 
 
 
